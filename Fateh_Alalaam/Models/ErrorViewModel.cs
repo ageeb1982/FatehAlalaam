d0d5dabce0d82e77726dcf79aaaeae9ce850bdfe
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2019-08-15 16:51:53 +0300

    Add project files.

---
+++ Fateh_Alalaam/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fateh_Alalaam.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
